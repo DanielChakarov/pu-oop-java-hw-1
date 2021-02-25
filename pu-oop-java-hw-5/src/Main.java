8c4a2d3e5a356724638a1862a29f8e3f58d81eb1
DanielChakarov <63393633+DanielChakarov@users.noreply.github.com>
2021-02-25 22:41:53 +0200

    Update Main.java

--- pu-oop-java-hw-5/src/Main.java
+++ pu-oop-java-hw-5/src/Main.java
@@ -1,7 +1,29 @@
-public class Main {
-    public static void main(String[] args) {
-        GameBoard gameBoard = new GameBoard();
-        gameBoard.gameBoard();
-        gameBoard.phoneCoondition();
+import java.awt.*;
+
+/**
+ * Class setting attributes for all pixels
+ */
+public class Pixel {
+
+    private int row;
+    private int col;
+    public static int pixType;
+    public static int pixSize = 9;
+    Color color;
+
+    public Pixel(int row, int col, int pixType,Color color){
+        this.row     = row;
+        this.col     = col;
+        this.pixType = pixType;
+        this.color   = color;
+
     }
+
+    public void render (Graphics g){
+        int tileX = this.col*pixSize;
+        int tileY = this.row*pixSize;
+        g.setColor(this.color);
+        g.fillRect(tileX,tileY,pixSize,pixSize);
+    }
+
 }
