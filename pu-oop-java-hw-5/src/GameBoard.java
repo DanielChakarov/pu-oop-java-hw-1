39757a2864bd9432bf891c06f658e33b48142cf7
DanielChakarov <63393633+DanielChakarov@users.noreply.github.com>
2021-02-25 22:21:49 +0200

    Update GameBoard.java

--- pu-oop-java-hw-5/src/GameBoard.java
+++ pu-oop-java-hw-5/src/GameBoard.java
@@ -6,10 +6,19 @@ import java.util.Random;
 
 public class GameBoard extends JFrame implements MouseListener {
 
-    Random random = new Random();
+    Random random         = new Random();
+    boolean isPhoneBroken = false;
+    int amountOfBrokenPix = 0;
     private Object[][] pixels;
 
+    String[] goodConditionPhone = new String[1];
+    String[] badConditionPhone  = new String[1];
+    char [] phoneNumber         = new char [10];
+    String serialNumberSymbols  = "wk48u3hh1k";
+
+
     public void gameBoard(){
+        this.pixels = new Object[64][64];
         Color color       = null;
         int typeOfPixels  = random.nextInt(3);
         int generatePixel = random.nextInt(3);
@@ -39,15 +48,16 @@ public class GameBoard extends JFrame implements MouseListener {
 
 
         this.setTitle("Pixels Game");
-        this.setSize(600,600);
+        this.setSize(585,585);
         this.setVisible(true);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.addMouseListener(this);
     }
 
+
     @Override
     public void paint(Graphics g) {
-        super.update(g);
+        super.paint(g);
         for (int row = 0; row < 64; row++){
             for (int col = 0; col < 64; col++){
                 Pixel p = (Pixel) this.getPixel(row,col);
@@ -60,9 +70,54 @@ public class GameBoard extends JFrame implements MouseListener {
         return this.pixels[row][col];
     }
 
+    private int coordinates(int pixCoordinates){
+        return pixCoordinates / Pixel.pixSize;
+    }
+
+
+
     @Override
     public void mouseClicked(MouseEvent e) {
+        System.out.println(this.coordinates((e.getX())));
+        System.out.println(this.coordinates((e.getY())));
 
+        int col     = this.coordinates(e.getY());
+        int row     = this.coordinates(e.getX());
+        Color color = null;
+
+        if (e.getClickCount() == 3){
+            Random random = new Random();
+            int ifPixIsBlack = random.nextInt(20);
+
+            if (ifPixIsBlack %2 == 0){
+                this.pixels[row][col] = new Pixel(row,col, Pixel.pixType,Color.BLACK);
+                this.repaint();
+                amountOfBrokenPix++;
+            }
+        }
+            if (amountOfBrokenPix >= 2024){
+                isPhoneBroken = true;
+            }
+    }
+
+    public  void phoneCoondition (){
+        if (isPhoneBroken){
+            badConditionPhone[0] = serialNumberSymbols;
+            System.out.println("The phone" +serialNumberSymbols+ "is broken");
+        }else {
+            goodConditionPhone[0] = serialNumberSymbols;
+            System.out.println("The phone" +serialNumberSymbols+ "is good");
+        }
+    }
+
+
+    public String serialNumberSetter(){
+        int randomNum;
+        for (int counter = 0; counter <= 10; counter++){
+            randomNum = this.random.nextInt(10);
+            this.phoneNumber[counter] = this.serialNumberSymbols.charAt(randomNum);
+        }
+        return new String(this.phoneNumber);
     }
 
     @Override
@@ -85,3 +140,4 @@ public class GameBoard extends JFrame implements MouseListener {
 
     }
 }
+
