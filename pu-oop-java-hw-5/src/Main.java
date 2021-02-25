0255f977efdb439a6c87e3396cccc97e637d5059
DanielChakarov <63393633+DanielChakarov@users.noreply.github.com>
2021-02-25 22:27:38 +0200

    Update Main.java

--- pu-oop-java-hw-5/src/Main.java
+++ pu-oop-java-hw-5/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         GameBoard gameBoard = new GameBoard();
+        gameBoard.gameBoard();
+        gameBoard.phoneCoondition();
     }
 }
